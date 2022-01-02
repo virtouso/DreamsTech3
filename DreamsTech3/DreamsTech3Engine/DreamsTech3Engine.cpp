f7444da78dc17d30bba5b6afc89dd9c1a58c91ea
LAPTOP-9KQD6BO4\s_moe <s.moeen.kamali@gmail.com>
2022-01-02 20:56:34 +0330

    started cmake project good for the best

--- DreamsTech3/DreamsTech3Engine/DreamsTech3Engine.cpp
+++ DreamsTech3/DreamsTech3Engine/DreamsTech3Engine.cpp
@@ -2,11 +2,47 @@
 //
 
 #include "DreamsTech3Engine.h"
+#include "imgui.h"
+#include "imgui-SFML.h"
 
-using namespace std;
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
 
 int main()
 {
-	cout << "Hello Dreams Tech." << endl;
+
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(window, event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
 	return 0;
 }
