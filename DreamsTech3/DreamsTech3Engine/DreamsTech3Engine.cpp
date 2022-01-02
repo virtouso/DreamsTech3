bc0a5b5cf8d93e3c4986fb14d6fccf07a61765a1
LAPTOP-9KQD6BO4\s_moe <s.moeen.kamali@gmail.com>
2022-01-02 15:51:52 +0330

    Init  Base Solution

---
+++ DreamsTech3/DreamsTech3Engine/DreamsTech3Engine.cpp
@@ -0,0 +1,12 @@
+﻿// DreamsTech3Engine.cpp : Defines the entry point for the application.
+//
+
+#include "DreamsTech3Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Dreams Tech." << endl;
+	return 0;
+}
---
+++ DreamsTech3/TestGame/TestGame.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+
+
+int main()
+{
+	std::cout << "Hello Test Game." <<std::endl;
+	return 0;
+}
